b7ec178b291a64db9ba9a574aa4b1fc9a132d874
Ankita <ankitakumawat111@gmail.com>
2023-12-09 14:22:06 +0530

    authentication

--- pages/store.js
+++ pages/store.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function Store() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (localStorage.getItem('loginStatus') !== 'true') {
+      router.push('/login');
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.setItem('loginStatus', 'false');
+    router.push('/login');
+  }
+
   return (
     <div className='store'>
       <h2>Welcome to the store!</h2>
-      <button className='logout-btn'>Logout</button>
+      <button className='logout-btn' onClick={handleLogout}>Logout</button>
     </div>
   );
 }
